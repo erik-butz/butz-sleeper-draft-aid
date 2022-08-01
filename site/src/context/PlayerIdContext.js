26ebfacab19a3d0120cf6eafccd5dc425de4df98
Erik Butz <erikbutzlive@gmail.com>
2022-08-01 14:26:48 -0500

    fixed clearing interval, cleaned up SQ problems (#17)

--- site/src/context/PlayerIdContext.js
+++ site/src/context/PlayerIdContext.js
@@ -7,30 +7,29 @@ export const PlayerProvider = ({ children }) => {
   const [url, setUrl] = useState('')
   const isMounted = useRef(false)
 
-  let newDraftedPlayerArray = []
-
-  const fetchDraftedUsers = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-
-    data.forEach((player) => {
-      if (!newDraftedPlayerArray.includes(player.player_id))
-        newDraftedPlayerArray.push(player.player_id)
-    })
-    setDraftedPlayerIds([...draftedPlayersId, ...newDraftedPlayerArray])
-  }
-
   useEffect(() => {
+    let newDraftedPlayerArray = []
+
+    const fetchDraftedUsers = async () => {
+      const response = await fetch(url)
+      const data = await response.json()
+
+      data.forEach((player) => {
+        if (!newDraftedPlayerArray.includes(player.player_id))
+          newDraftedPlayerArray.push(player.player_id)
+      })
+      setDraftedPlayerIds([...draftedPlayersId, ...newDraftedPlayerArray])
+    }
+
     if (isMounted.current) {
-      //Need to figure out how to cancel this if a new draft id is submitted
-      //Is there a better way to constantly fetch the live draft data besides interval every 1s?
-      setInterval(() => {
+      const interval = setInterval(() => {
         fetchDraftedUsers()
       }, 1000)
+      return () => clearInterval(interval)
     } else {
       isMounted.current = true
     }
-  }, [url])
+  }, [draftedPlayersId, url])
 
   const setSleeperDraftId = (sleeperDraftId) => {
     setUrl(`https://api.sleeper.app/v1/draft/${sleeperDraftId}/picks`)
